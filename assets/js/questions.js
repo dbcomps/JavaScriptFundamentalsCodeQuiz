c66793fe7619392cb37e5820ec84081f5a5bab49
David Barraza <dbcomps@mac.com>
2022-08-09 18:46:19 -0500

    I need to learn arrays

---
+++ assets/js/questions.js
@@ -0,0 +1,20 @@
+// add questions here
+
+var questions = [{
+		question: "What is JavaScript?",
+		answers: [
+		"A superset of Java",
+		"A language for making the style and presentation of a webpage",
+		"A programming language to implement the logic of a webpage "
+		],
+		correctAnswerIndex: 2},
+{
+		question: "The id attribute of an element can be used on how many different elements?",
+		answers: [
+		"1",
+		"4",
+		"16",
+		"32"
+		],
+		correctAnswerIndex: 2}
+];
