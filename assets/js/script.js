5b982593ed1c6368c38f328bd8963793c6429799
David Barraza <dbcomps@mac.com>
2022-08-09 16:40:08 -0500

    countdown timer working, next link to start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,7 @@
 var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('display');
-
-var message =
-  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-var words = message.split(' ');
-
 
 function countdown() {
-	var timeLeft = 5;
+	var timeLeft = 100;
 
  var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
@@ -24,17 +18,5 @@ function countdown() {
   }, 1000);
 }
 
-function displayMessage() {
-  var wordCount = 0;
-
-  var msgInterval = setInterval(function () {
-    if (words[wordCount] === undefined) {
-      clearInterval(msgInterval);
-    } else {
-      mainEl.textContent = words[wordCount];
-      wordCount++;
-    }
-  }, 1000);
-}
 
 countdown();
